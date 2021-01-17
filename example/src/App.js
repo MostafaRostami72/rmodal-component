76d3bda690e5fd0086953f368eeddf9c02f5b0b1
mostafa <m.rostamimahmodi72@gmail.com>
2021-01-17 13:04:20 +0330

    improve modal styles

--- example/src/App.js
+++ example/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
         <div>
             <button onClick={() => setShow(true)}>show</button>
 
-            <Modal show={show} onClose={() => setShow(false)}>
+            <Modal show={show} onClose={() => setShow(false)} smModal={true} modalTitle={"modal Title"}>
 
                 <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.</p>
                 <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.</p>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, {useRef, useEffect, useState} from 'react';
 import styled from 'styled-components';
 import {SlideInBottomAnimate, SlideInBottomAnimateMobile, SlideOutBottomAnimate, SlideOutBottomAnimateMobile} from "./components/animations";
 
-const Portal = ({show, children, onClose}) => {
+const Portal = ({show, children, onClose, modalTitle, smModal = false}) => {
     const containerRef = useRef('');
     const bodyRef = useRef('');
     const contentRef = useRef('');
@@ -30,32 +30,40 @@ const Portal = ({show, children, onClose}) => {
     const handleDragEnd = (event) => {
         event.preventDefault();
         if (window.innerWidth < 767) {
-            const {pageY, pageX} = event.changedTouches[0];
+            const {clientY, pageX} = event.changedTouches[0];
             const contentHeight = contentRef.current.clientHeight;
-            const allowDray = (window.innerHeight - (contentHeight / 2)) + 10;
-            if (pageY > allowDray) {
+            const windowsHeight = window.innerHeight;
+            const allowDray = (windowsHeight - (contentHeight / 2)) + 10;
+
+            if (clientY > allowDray) {
                 handleClose();
             } else {
-                bodyRef.current.style.top = 'calc(100% - ' + contentHeight + 'px)';
+                if (contentHeight < windowsHeight) {
+                    bodyRef.current.style.height = contentHeight + 'px';
+                } else {
+                    bodyRef.current.style.height = 'calc(100vh - 80px)';
+                }
             }
         }
     };
 
     const handleDrag = (event) => {
         event.preventDefault();
+
         if (window.innerWidth < 767) {
-            const {pageY, pageX} = event.touches[0];
+            const {clientY, pageX} = event.touches[0];
             const contentHeight = contentRef.current.clientHeight;
 
-            if ((window.innerHeight - contentHeight) <= pageY) {
-                bodyRef.current.style.top = pageY + 'px';
+            if ((window.innerHeight - contentHeight) <= (clientY - 80)) {
+                bodyRef.current.style.height = 'calc(100vh - ' + clientY + 'px)';
             }
         }
     };
 
     useEffect(() => {
-        headerRef.current.addEventListener('touchend', handleDragEnd);
-        headerRef.current.addEventListener('touchmove', handleDrag);
+        headerRef.current.addEventListener('touchend', handleDragEnd, false);
+        headerRef.current.addEventListener('touchmove', handleDrag, false);
+
         return () => {
             headerRef.current.removeEventListener('touchend', handleDrag);
             headerRef.current.removeEventListener('touchmove', handleDrag);
@@ -63,33 +71,40 @@ const Portal = ({show, children, onClose}) => {
     }, []);
 
     useEffect(() => {
-        if (window.innerWidth < 767) {
-            const contentHeight = contentRef.current.clientHeight;
-            const windowsHeight = window.innerHeight;
+        const contentHeight = contentRef.current.clientHeight;
 
+        if (window.innerWidth < 768) {
+            const windowsHeight = window.innerHeight;
             if (contentHeight < windowsHeight) {
-                bodyRef.current.style.top = 'calc(100% - ' + contentHeight + 'px)';
+                bodyRef.current.style.height = contentHeight + 'px';
             } else {
-                bodyRef.current.style.top = '80px';
+                bodyRef.current.style.height = 'calc(100vh - 80px)';
             }
         }
-    }, [show]);
+    });
 
     return (
         <Container ref={containerRef} className="r-modal-component">
             <Backdrop ref={backdropRef} onClick={(e) => handleClose(e)} className={"fade " + (show ? 'in' : '')}/>
 
-            <Body ref={bodyRef} role="dialog" className={'rmc-dialog modal-dialog-scrollable bottom ' + (show ? 'show-modal slide-in' : 'slide-out')}>
-                <Dialog>
-                    <DialogContent ref={contentRef}>
+            <Body ref={bodyRef} role="dialog" className={'modal-dialog-scrollable bottom ' + (show ? 'show-modal slide-in ' : 'slide-out ') +(smModal ? 'modal-sm ' : '')}>
+                <Dialog className="rmc-dialog">
+                    <DialogContent className="rmc-dialog-content" ref={contentRef}>
                         <Header ref={headerRef} className="rmc-header">
                             <CloseLine className="rmc-header-close-line"/>
 
                             <ModalClose className="rmc-header-close" onClick={(e) => handleClose(e)}>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11.89 11.89"><g id="Layer_2" data-name="Layer 2"><g id="Layer_2-2" data-name="Layer 2"><line x1="0.53" y1="0.53" x2="11.36" y2="11.36" fill="none" stroke="#222" stroke-linejoin="round" stroke-width="1.5"/><line x1="11.36" y1="0.53" x2="0.53" y2="11.36" fill="none" stroke="#222" stroke-linejoin="round" stroke-width="1.5"/></g></g></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11.89 11.89">
+                                    <g id="Layer_2" data-name="Layer 2">
+                                        <g id="Layer_2-2" data-name="Layer 2">
+                                            <line x1="0.53" y1="0.53" x2="11.36" y2="11.36" fill="none" stroke="#222" stroke-linejoin="round" stroke-width="1.5"/>
+                                            <line x1="11.36" y1="0.53" x2="0.53" y2="11.36" fill="none" stroke="#222" stroke-linejoin="round" stroke-width="1.5"/>
+                                        </g>
+                                    </g>
+                                </svg>
                             </ModalClose>
 
-                            <h4 className="rmc-title">Modal Title</h4>
+                            <h4 className="rmc-title">{modalTitle}</h4>
                         </Header>
 
 
@@ -185,18 +200,25 @@ const DialogBody = styled.div`
 `;
 
 const DialogContent = styled.div`
-    position: absolute;
+    position: relative;
     background-color: #fff;
     outline: 0;
     width: 100%;
     -webkit-box-shadow: 0 4px 4px rgba(0,0,0,.12), 0 0 10px rgba(0,0,0,.06);
     box-shadow: 0 4px 4px rgba(0,0,0,.12), 0 0 10px rgba(0,0,0,.06);
-    border-radius: 8px;
-    padding: 0;
+    border-radius: 16px;
+    padding-bottom: 24px;
     
-    @media (max-width: 767px) {
-        border-radius: 16px 16px 0 0;
-        padding: 0 0 16px 0;
+    @media (min-width: 767px) {
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    @media (max-width: 768px) {
+        border-radius: 24px 24px 0 0;
+        padding: 0;
+        bottom: unset;
     }
 `;
 
@@ -218,13 +240,29 @@ const Body = styled.div`
     
     @media (max-width: 767px) {
         left: 0;
+        bottom: 0;
         height: auto;
+        max-width: 100%;
+    }
+    
+    &.modal-sm {
+        width: 370px;
+
+        @media (max-width: 768px) {
+            width: 100%;
+        }
     }
     
     -webkit-animation-duration: .3s;
     animation-duration: .3s;
     -webkit-animation-fill-mode: forwards;
     animation-fill-mode: forwards;
+        
+    @media (min-width: 767px) {
+        height: 100%;
+        max-height: calc(100vh - 8rem);
+        top: 50%;
+    }
         
     &.slide-in {
         -webkit-animation-timing-function: ease-in-out;
@@ -254,7 +292,6 @@ const Body = styled.div`
     
     &.modal-dialog-scrollable {
          display: flex;
-         max-height: calc(100% - 1rem);
     }
 `;
 
@@ -271,6 +308,10 @@ const Header = styled.div`
     
     @media (max-width: 768px) {
         flex-direction: column;
+    }
+    
+    .rmc-title {
+        margin-bottom: 0;
     }
 `;
 
