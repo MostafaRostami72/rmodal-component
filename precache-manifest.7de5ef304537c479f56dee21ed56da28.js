90029248882d8426adb4eded2e5c64ad22de3364
mostafa <m.rostamimahmodi72@gmail.com>
2021-01-17 14:28:21 +0330

    Updates

---
+++ precache-manifest.7de5ef304537c479f56dee21ed56da28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af600bae4daa4b5f4498bdf7ad19d6da",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bd48e1f08a503f76e2ae",
+    "url": "./static/css/2.032730b7.chunk.css"
+  },
+  {
+    "revision": "bd48e1f08a503f76e2ae",
+    "url": "./static/js/2.8d835a29.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.8d835a29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc54d42ed5e570253270",
+    "url": "./static/js/main.28a7e38a.chunk.js"
+  },
+  {
+    "revision": "e3f05d033bae37170092",
+    "url": "./static/js/runtime-main.546e9782.js"
+  }
+]);
